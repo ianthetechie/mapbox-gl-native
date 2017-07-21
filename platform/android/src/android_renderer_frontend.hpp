bc6e1bcbb2c4c4a765d02bfc36904d0525593847
Ivo van Dongen <info@ivovandongen.nl>
2017-07-21 16:15:30 +0300

    [android] async rendering
    - introduces GLSurfaceView
    - introduces Orchestration thread
    - renders on the gl thread

--- platform/android/src/android_renderer_frontend.hpp
+++ platform/android/src/android_renderer_frontend.hpp
@@ -5,32 +5,48 @@
 #include <mbgl/renderer/renderer_frontend.hpp>
 #include <mbgl/util/async_task.hpp>
 #include <mbgl/util/geo.hpp>
+#include <mbgl/util/image.hpp>
+#include <mbgl/util/run_loop.hpp>
 
 #include <functional>
 #include <memory>
 #include <vector>
+#include <string>
 
 namespace mbgl {
 
 class View;
 class Renderer;
-class View;
+class FileSource;
+class Scheduler;
 class RenderedQueryOptions;
 class SourceQueryOptions;
 
+namespace util {
+template <class> class Thread;
+} // namespace util
+
 namespace android {
+
+class AndroidRendererBackend;
 
 class AndroidRendererFrontend : public RendererFrontend {
 public:
     using InvalidateCallback = std::function<void ()>;
-    AndroidRendererFrontend(std::unique_ptr<Renderer>, InvalidateCallback);
+    AndroidRendererFrontend(float pixelRatio,
+                            mbgl::FileSource&,
+                            mbgl::Scheduler&,
+                            std::string programCacheDir,
+                            InvalidateCallback);
     ~AndroidRendererFrontend() override;
 
     void reset() override;
     void setObserver(RendererObserver&) override;
 
     void update(std::shared_ptr<UpdateParameters>) override;
-    void render(View& view);
+
+    // Called from OpenGL Thread
+    void render();
 
     // Feature querying
     std::vector<Feature> queryRenderedFeatures(const ScreenCoordinate&, const RenderedQueryOptions&) const;
@@ -38,13 +54,27 @@ public:
     std::vector<Feature> querySourceFeatures(const std::string& sourceID, const SourceQueryOptions&) const;
     AnnotationIDs queryPointAnnotations(const ScreenBox& box) const;
 
+    // RenderBackend proxy - Called from OpenGL Thread
+    void resizeFramebuffer(int width, int height);
+
     // Memory
     void onLowMemory();
 
+    // Snapshot - Callback will be called on calling thread through RunLoop
+    using SnapshotCallback = std::function<void (PremultipliedImage)>;
+    void requestSnapshot(SnapshotCallback);
+
 private:
-    std::unique_ptr<Renderer> renderer;
+    std::unique_ptr<AndroidRendererBackend> backend;
+    std::unique_ptr<util::Thread<Renderer>> renderer;
+    std::unique_ptr<RendererObserver> rendererObserver;
     std::shared_ptr<UpdateParameters> updateParameters;
     util::AsyncTask asyncInvalidate;
+
+    util::RunLoop* mapRunLoop;
+
+    bool framebufferSizeChanged = true;
+    std::unique_ptr<SnapshotCallback> snapshotCallback;
 };
 
 } // namespace android
